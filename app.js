b7bebd68b582815c7c42614b2d55307f7ade0452
Chinmay Atrawalkar <catrawalkar@gmail.com>
2019-10-24 12:46:38 +0530

    'final'

--- app.js
+++ app.js
@@ -3,53 +3,59 @@ const app = express();
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const url =
-  "http://results.eci.gov.in/ACOCT2019/ConstituencywiseS13254.htm?ac=254";
+// const url =
+//   "http://results.eci.gov.in/ACOCT2019/ConstituencywiseS13254.htm?ac=254";
 
-axios(url)
-  .then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
+function getData(url) {
+  return axios("http://results.eci.gov.in/ACOCT2019/" + url)
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const eciTable = $("#div1 > table").first();
+      const tr = eciTable.find("tbody > tr[style='font-size:12px;']");
+      let constituency = [];
 
-    const eciTable = $("#div1 > table").first();
+      tr.each(function() {
+        //   const td = $(this).find("td");
+        let candidate = {};
+        candidate["O.S.N"] = $(this)
+          .find("td:nth-child(1)")
+          .text();
+        candidate["Candidate"] = $(this)
+          .find("td:nth-child(2)")
+          .text();
+        candidate["Party"] = $(this)
+          .find("td:nth-child(3)")
+          .text();
+        candidate["EVM Votes"] = $(this)
+          .find("td:nth-child(4)")
+          .text();
+        candidate["Postal Votes"] = $(this)
+          .find("td:nth-child(5)")
+          .text();
+        candidate["Total Votes"] = $(this)
+          .find("td:nth-child(6)")
+          .text();
+        candidate["% of Votes"] = $(this)
+          .find("td:nth-child(7)")
+          .text();
+        constituency.push(candidate);
+      });
 
-    const tr = eciTable.find("tbody > tr[style='font-size:12px;']");
-
-    let constituency = [];
-
-    tr.each(function() {
-      //   const td = $(this).find("td");
-      let candidate = {};
-      candidate["O.S.N"] = $(this)
-        .find("td:nth-child(1)")
-        .text();
-      candidate["Candidate"] = $(this)
-        .find("td:nth-child(2)")
-        .text();
-      candidate["Party"] = $(this)
-        .find("td:nth-child(3)")
-        .text();
-      candidate["EVM Votes"] = $(this)
-        .find("td:nth-child(4)")
-        .text();
-      candidate["Postal Votes"] = $(this)
-        .find("td:nth-child(5)")
-        .text();
-      candidate["Total Votes"] = $(this)
-        .find("td:nth-child(6)")
-        .text();
-      candidate["% of Votes"] = $(this)
-        .find("td:nth-child(7)")
-        .text();
-      constituency.push(candidate);
+      return constituency;
+    })
+    .catch(error => {
+      console.log(error);
     });
-
-    console.log(constituency);
-  })
-  .catch(console.error);
+}
 
 app.get("/", function(req, res) {
   res.status(200).send("Welcome to our restful API");
+});
+
+app.get("/constituency/:url", async function(req, res) {
+  const response = await getData(req.params.url);
+  res.status(200).send(response);
 });
 
 app.listen(3000, function() {
