0c75ea59e1246942cbebb7f0f939ede7e82efed7
Chinmay Atrawalkar <catrawalkar@gmail.com>
2019-10-24 14:02:04 +0530

    ''

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const cheerio = require("cheerio");
 
 // const url =
 //   "http://results.eci.gov.in/ACOCT2019/ConstituencywiseS13254.htm?ac=254";
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD");
+  // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 function getData(url) {
   return axios("http://results.eci.gov.in/ACOCT2019/" + url)
