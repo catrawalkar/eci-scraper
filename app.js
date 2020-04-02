2e12528d0b63241a8f981863b63824f4317b174b
chinmay-ubuntu <chinmay@gmail.com>
2020-04-02 15:41:48 +0530

    data is being fetched

--- app.js
+++ app.js
@@ -56,53 +56,59 @@ function getCovidData() {
       const $ = cheerio.load(html);
       const section = $(".elementor-element-51bcd0e");
       // console.log(section);
-      const confirmed = $("#sns_global_scripts")
-        .map((i, x) => x.children[0])
-        .filter((i, x) => {
-          console.log(x.data);
-          return x && x.data.match(/document/);
-        });
-      console.log(confirmed);
+      const scriptTag = $("#sns_global_scripts").html();
+      // .filter((i, x) => {
+      //   // console.log(x.data.match(/document.getElementById\("[a-z]{5,8}/g));
+      //   return x.data.match(/document.getElementById\("[a-z]{5,8}/g);
+      // });
       // .get(0);
 
-      // console.log(confirmed);
-      // const tr = eciTable.find("tbody > tr[style='font-size:12px;']");
-      // let constituency = [];
+      const confirmed = scriptTag
+        .match(/document.getElementById\("cases"\).innerHTML = "[0-9]+"/g)[0]
+        .match(/[0-9]+/g)[0];
 
-      // tr.each(function() {
-      //   let candidate = {};
-      //   candidate["O.S.N"] = $(this)
-      //     .find("td:nth-child(1)")
-      //     .text();
-      //   candidate["Candidate"] = $(this)
-      //     .find("td:nth-child(2)")
-      //     .text();
-      //   candidate["Party"] = $(this)
-      //     .find("td:nth-child(3)")
-      //     .text();
-      //   candidate["EVM Votes"] = $(this)
-      //     .find("td:nth-child(4)")
-      //     .text();
-      //   candidate["Postal Votes"] = $(this)
-      //     .find("td:nth-child(5)")
-      //     .text();
-      //   candidate["Total Votes"] = $(this)
-      //     .find("td:nth-child(6)")
-      //     .text();
-      //   candidate["% of Votes"] = $(this)
-      //     .find("td:nth-child(7)")
-      //     .text();
-      //   constituency.push(candidate);
-      // });
+      const casesaspermohfw = scriptTag
+        .match(/document.getElementById\("mohfw"\).innerHTML = "[0-9]+"/g)[0]
+        .match(/[0-9]+/g)[0];
 
-      return null;
+      const deaths = scriptTag
+        .match(/document.getElementById\("death"\).innerHTML = "[0-9]+"/g)[0]
+        .match(/[0-9]+/g)[0];
+
+      const recovered = scriptTag
+        .match(/document.getElementById\("recovery"\).innerHTML = "[0-9]+"/g)[0]
+        .match(/[0-9]+/g)[0];
+
+      const treatment_ongoing = scriptTag
+        .match(/document.getElementById\("ongoing"\).innerHTML = "[0-9]+"/g)[0]
+        .match(/[0-9]+/g)[0];
+
+      const nooftestsdone = scriptTag
+        .match(/document.getElementById\("testnum"\).innerHTML = "[0-9]+"/g)[0]
+        .match(/[0-9]+/g)[0];
+
+      const lineone =
+        "Cases updated 2-Apr, 02:01 pm; Tests as of 01-Apr; next update 04:00 pm; Sources: MoHFW, Worldometers, ICMR, JHU ";
+      const linetwo =
+        "*Sources: Ministry of Health and Family Welfare, Worldometers, JHU, BNO or crowdsourcing with verification of official sources; MoHFW data: Cases = 1965, Recoveries = 150, Deaths = 50. Testing source: ICMR";
+
+      return {
+        confirmed,
+        casesaspermohfw,
+        deaths,
+        recovered,
+        treatment_ongoing,
+        nooftestsdone,
+        lineone,
+        linetwo
+      };
     })
     .catch(error => {
       console.log(error);
     });
 }
 
-getCovidData();
+const response = getCovidData();
 
 app.get("/", function(req, res) {
   res.status(200).send("Welcome to our restful API");
@@ -111,22 +117,12 @@ app.get("/", function(req, res) {
 app.get("/covid19/in", async function(req, res) {
   const response = await getCovidData();
 
-  res.status(200).send({
-    confirmed: 2094,
-    casesaspermohfw: 1965,
-    deaths: 58,
-    recovered: 171,
-    treatment_ongoing: 1865,
-    nooftestsdone: 47951,
-    lineone:
-      "Cases updated 2-Apr, 11:59 am; Tests as of 01-Apr; next update 02:00 pm; Sources: MoHFW, Worldometers, ICMR, JHU",
-    linetwo:
-      "*Sources: Ministry of Health and Family Welfare, Worldometers, JHU, BNO or crowdsourcing with verification of official sources; MoHFW data: Cases = 1965, Recoveries = 150, Deaths = 50. Testing source: ICMR"
-  });
+  res.status(200).send(response);
 });
 
 app.get("/constituency/:url", async function(req, res) {
   const response = await getData(req.params.url);
+  console.log(response);
   res.status(200).send(response);
 });
 
