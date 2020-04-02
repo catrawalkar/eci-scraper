6d5a2c3896c6720a74116e36ae769564e51f3dfb
chinmay-ubuntu <chinmay@gmail.com>
2020-04-02 12:30:49 +0530

    covid added

--- app.js
+++ app.js
@@ -49,8 +49,69 @@ function getData(url) {
     });
 }
 
+function getCovidData() {
+  return axios("https://covidindia.org/")
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const section = $(".elementor-element-51bcd0e");
+      // console.log(section);
+      const confirmed = $("#c3183e7").html();
+
+      console.log(section);
+      // const tr = eciTable.find("tbody > tr[style='font-size:12px;']");
+      // let constituency = [];
+
+      // tr.each(function() {
+      //   let candidate = {};
+      //   candidate["O.S.N"] = $(this)
+      //     .find("td:nth-child(1)")
+      //     .text();
+      //   candidate["Candidate"] = $(this)
+      //     .find("td:nth-child(2)")
+      //     .text();
+      //   candidate["Party"] = $(this)
+      //     .find("td:nth-child(3)")
+      //     .text();
+      //   candidate["EVM Votes"] = $(this)
+      //     .find("td:nth-child(4)")
+      //     .text();
+      //   candidate["Postal Votes"] = $(this)
+      //     .find("td:nth-child(5)")
+      //     .text();
+      //   candidate["Total Votes"] = $(this)
+      //     .find("td:nth-child(6)")
+      //     .text();
+      //   candidate["% of Votes"] = $(this)
+      //     .find("td:nth-child(7)")
+      //     .text();
+      //   constituency.push(candidate);
+      // });
+
+      return null;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+getCovidData();
+
 app.get("/", function(req, res) {
   res.status(200).send("Welcome to our restful API");
+});
+
+app.get("/covid19/in", async function(req, res) {
+  const response = await getCovidData();
+
+  res.status(200).send({
+    confirmed: 2032,
+    cases_as_per_mohfw: 1965,
+    deaths: 58,
+    recovered: 171,
+    treatment_ongoing: 1803,
+    no_of_tests_done: 47951
+  });
 });
 
 app.get("/constituency/:url", async function(req, res) {
